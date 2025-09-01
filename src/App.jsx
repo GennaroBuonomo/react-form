ad2b009db8c97add64d2537a96c1546a24e50740
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-01 15:35:17 +0200

    fine primo millestone

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 
-const articoli = [
-  'Il Signore degli Anelli',
-  'Lo Squalo',
-  'Il Piccolo Principe',
-  'Orgoglio e Pregiudizio',
-  'Harry Potter e la Pietra Filosofale',
+const initialArticles = [
+  "Il Signore degli Anelli",
+  "Lo Squalo",
+  "Il Piccolo Principe",
+  "Orgoglio e Pregiudizio",
+  "Harry Potter e la Pietra Filosofale",
 ];
 
 function App() {
   return (
+    <> 
     <div className="container my-5">
       <div className="row">
-        <div className="col-12">
-          <ul className="list-unstyled">
-            {articoli.map((titolo, index) => (
-              <li key={index}>{titolo}</li>
-            ))}
-          </ul>
+        <div className="col-12 gy-3">
+          <h1>Lista articoli</h1>
+          <div className="col-12">
+            <ul className="list-unstyled list-group ms-0">
+              {initialArticles.map((article,index) => {
+                return (
+                  <li className="list-group-item" key={index}>
+                    {article}
+                  </li>
+                );                
+                })}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
+    </>
   )
 }
 
